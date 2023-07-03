9e3842d6ec9c117f9dd2e5a6d7e480fc6b8f30d0
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-03 11:11:37 -0700

    add Data in Console

--- EcommerenceApp/src/navigation/AppNavigation.js
+++ EcommerenceApp/src/navigation/AppNavigation.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Frontend/Home';
 import Products from '../screens/Frontend/Products';
 
 
-const Stack = createNativeStackNavigator()
+// const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator();
+
+
 export default function AppNavigation() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name='Home' component={Home}/>
-            <Stack.Screen name='Products' component={Products}/>
-        </Stack.Navigator>
+        <Tab.Navigator>
+            <Tab.Screen name='Home' component={Home}/>
+            <Tab.Screen name='Products' component={Products}/>
+        </Tab.Navigator>
     </NavigationContainer>
   )
 }
--- EcommerenceApp/src/screens/Frontend/Home.js
+++ EcommerenceApp/src/screens/Frontend/Home.js
@@ -1,25 +1,100 @@
-import React from 'react'
-import { View, Text, StyleSheet} from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { TextInput,Button } from 'react-native-paper';
+
+
+const initialState = {
+  title:"",
+  description:"",
+  sortOrder:"123"
+}
 
 export default function Home() {
+
+  const [state, setState]  = useState(initialState)
+
+  const handleChange = (name, value) => {
+    setState(s=>({...s,[name]: value}))
+  }
+
+  const addProduct = () =>{
+
+    let {title, description, sortOrder} = state
+    console.log(sortOrder)
+    title=title.trim()
+    description=description.trim()
+
+    let formData = {title, description,sortOrder}
+
+    formData.id= Math.random().toString(36).slice(2)
+    formData.dateCreated = 'firebase.firestore.Fieldvalue.serverTimeStamp()'
+    formData.SortOrder = Number(sortOrder)
+    formData.status = 'active'
+    formData.createdBy = {
+      email: 'user.email',
+      uid:'user.uid'
+    }
+    createDocunment(formData)
+  }
+  
+  const createDocunment=(formData)=>{
+    console.log(formData)
+  
+}
+
+
   return (
-    <View style={Styles.container}>
-      <Text style={Styles.heading}>Ecommerence Home Page</Text>
-    </View>
+    <SafeAreaView style={Styles.container}>
+
+      <View style={Styles.formcontainer}>
+        <TextInput
+          mode="outlined"
+          label="Title "
+          placeholder="Type something"
+          name='title'
+          onChangeText={value=>{handleChange("title",value)}}
+          />
+          
+       </View>
+
+      <View style={Styles.formcontainer}>
+      <TextInput
+          mode="outlined"
+          label="Description"
+          placeholder="Type something" 
+          name='description'
+          onChangeText={value=>{handleChange("description",value)}}
+          />
+       </View>
+
+      <View style={Styles.formcontainer}>
+      <TextInput
+          mode="outlined"
+          label="Sort Order"
+          placeholder="Type something" 
+          name="sortOrder"
+          onChangeText={value=>{handleChange("sortOrder",value)}}
+          keyboardType="number-pad"
+          />
+       </View>
+       <View>
+       <Button icon="plus" mode="contained" onPress={addProduct}>Add</Button>
+       </View>
+    </SafeAreaView>
   )
 }
 
 const Styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    justifyContent:'center',
-    alignItems:"center",
-    backgroundColor:'lightyellow'
-  },
-  heading:{
-    color:"green",
-    fontSize: 30,
-    fontWeight:'bold'
+    justifyContent: 'center',
+    // alignItems:"center",
+    backgroundColor: 'lightyellow',
+    paddingHorizontal: 15
 
+  },
+  formcontainer:{
+    marginBottom: 10
   }
+
 })
